823f0e16eedad192e4bb1ccdbc88adb20e29c117
Clarence <clarence.duodu.ext@heidelbergmaterials.com>
2026-07-10 14:30:20 +0000

    Add Logs for Auth Routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { authController } from "../controllers/authController";
-import { digiHRService } from "../services/digiHRService";
 
 const router = Router();
 
+console.log("Auth Routes rendered")
 router.post("/login", authController.loginController);
 router.post("/register", authController.signUpController);
 router.post("/onboarding", authController.onBoardingController)
 router.post("/logout", authController.logOutController);
 router.post("/refresh", authController.refreshController);
 router.post("/sync", authController.syncUserController);
+
 export default router;
