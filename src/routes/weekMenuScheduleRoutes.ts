8915a4e75d1508461cbbe35caf19a3514da14a5c
Clarence <clarence.duodu.ext@heidelbergmaterials.com>
2026-07-29 09:04:54 +0000

    Reorder WeekMenuSchedule Routes

--- src/routes/weekMenuScheduleRoutes.ts
+++ src/routes/weekMenuScheduleRoutes.ts
@@ -4,10 +4,10 @@ import { weekMenuScheduleController } from "../controllers/weekMenuScheduleContr
 const router = Router();
 
 router.get("/", weekMenuScheduleController.getAllWeekMenuSchedulesController);
-router.get("/:id", weekMenuScheduleController.getWeekMenuScheduleByIdController);
 router.get("/by-week-year", weekMenuScheduleController.getWeekMenuScheduleByWeekAndYearController);
 router.get("/by-menu", weekMenuScheduleController.getWeekMenuSchedulesByMenuController);
 router.post("/", weekMenuScheduleController.createWeekMenuScheduleController);
+router.get("/:id", weekMenuScheduleController.getWeekMenuScheduleByIdController);
 router.put("/:id", weekMenuScheduleController.updateWeekMenuScheduleController);
 
 export default router;
