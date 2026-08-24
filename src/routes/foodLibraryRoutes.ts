74db40cd2532a42c2a404d8e25d6edd700f0a228
Clarence <clarence.duodu.ext@heidelbergmaterials.com>
2026-08-24 09:48:37 +0000

    Modify Food Library Permissions

--- src/routes/foodLibraryRoutes.ts
+++ src/routes/foodLibraryRoutes.ts
@@ -5,7 +5,7 @@ import { Roles } from "../enums/ERoles";
 
 const router = Router();
 
-router.get("/", authenticate, authorize([Roles.admin, Roles.hr]), foodLibraryController.getAllFoodItemsController);
+router.get("/", authenticate, authorize([Roles.admin, Roles.hr, Roles.user]), foodLibraryController.getAllFoodItemsController);
 router.get("/:foodGroup", authenticate, authorize([Roles.admin, Roles.hr]), foodLibraryController.getFoodItemsByFoodGroupsController);
 router.post("/batch", authenticate, authorize([Roles.admin, Roles.hr]), foodLibraryController.createFoodItemsBatch);
 router.post("/", authenticate, authorize([Roles.admin, Roles.hr]), foodLibraryController.createFoodItem);
